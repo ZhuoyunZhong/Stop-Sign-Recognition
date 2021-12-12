12d5cb007fbd4326ec402aa7f9ed4845eaabe5be
Preyashb <73863651+Preyashb@users.noreply.github.com>
2021-12-12 15:13:57 -0500

    Works with data_reader now

--- haar_cascade.py
+++ haar_cascade.py
@@ -1,55 +1,54 @@
 import cv2 
 import matplotlib.pyplot as plt 
+import time
 
 from data_reader import DataReader
 
 # https://benchmark.ini.rub.de/?section=gtsrb&subsection=dataset
 
 
-data = DataReader()
-data.load_dataset("lisa")
+def haar_cascade(img_gray):
+    '''
+    Parameters:
+    gray_img (2d numpy array): Input gray scale image
+
+    Returns:
+    a list of found stop signs areas [(x, y, height, weight), ...]
+    if nothing is found, return an empty list []
+    '''
+    found = []
+    x, y, height, weight = [], [], [], []
+    stop_data = cv2.CascadeClassifier('Stopsign_HAAR_19Stages.xml')
+    results = stop_data.detectMultiScale(img_gray) 
+    if(len(results) == 0): 
+        found = [] 
+        return found
+    for i in results:
+        found.append(i)
+    return found
+
+    
+
+def main():
+    data = DataReader()
+
+    # Test with LISA dataset
+    data.load_dataset("lisa")
+    tick = time.time()
+    accuracy = data.run_test(haar_cascade)
+    print("The running time is ", time.time()-tick)
+    print("The accuracy of running LISA dataset is: ", accuracy)
+    print("Accurate Indices: ", data.accurate_indices)
+    data.visualize_result()
+
+    # Test with online dataset
+    data.load_dataset("online")
+    accuracy = data.run_test(haar_cascade)
+    print("The running time is ", time.time()-tick)
+    print("The accuracy of running online dataset is: ", accuracy)
+    print("Accurate Indices: ", data.accurate_indices)
+    data.visualize_result()
 
 
-# Opening image 
-# img = cv2.imread("data_set/image.jpg") 
-img = cv2.imread("data_set/37.JPG") 
-
-# OpenCV opens images as BRG  
-# but we want it as RGB We'll  
-# also need a grayscale version 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-
-
-# Use minSize because for not  
-# bothering with extra-small  
-# dots that would look like STOP signs 
-
-# stop_data = cv2.CascadeClassifier('stop_data.xml') 
-stop_data = cv2.CascadeClassifier('Stopsign_HAAR_19Stages.xml') 
-  
-
-found = stop_data.detectMultiScale(img_gray, minSize =(20, 20)) 
-# Don't do anything if there's  
-# no sign 
-
-if(found is None): print("Empty")
-# print("Found? ", found )
-
-amount_found = len(found) 
-if amount_found != 0: 
-    # There may be more than one 
-    # sign in the image 
-    for (x, y, width, height) in found: 
-        # We draw a green rectangle around 
-        # every recognized sign 
-        cv2.rectangle(img_rgb, (x, y),  
-                      (x + height, y + width),  
-                      (0, 255, 0), 5) 
-
-          
-# Creates the environment of  
-# the picture and shows it 
-plt.subplot(1, 1, 1) 
-plt.imshow(img_rgb) 
-plt.show() 
+if __name__ == "__main__":
+    main()
