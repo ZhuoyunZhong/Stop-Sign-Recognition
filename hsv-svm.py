7f5b3a0d65213f7fad14f325c6b91906572af3b1
ZhuoyunZhong <zzhong3@wpi.edu>
2021-12-14 20:54:38 -0500

    hsv extraction

--- hsv-svm.py
+++ hsv-svm.py
@@ -1,4 +1,5 @@
 import cv2 
+import numpy as np
 import matplotlib.pyplot as plt 
 import time
 
@@ -6,21 +7,66 @@ from data_reader import DataReader
 
 
 def pre_processing(img):
-    img = cv2.medianBlur(img.copy())
+    img = cv2.medianBlur(img, 5)
     
+    # HSV
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Red color range to find the traffic sign
+    #low_b_range = np.array([90, 100, 70])
+    #up_b_range = np.array([130, 255, 255])
+    low_r_range1 = np.array([0, 100, 70])
+    up_r_range1 = np.array([10, 255, 255])
+    low_r_range2 = np.array([160, 100, 70])
+    up_r_range2 = np.array([180, 255, 255])
+    # Find the area in range
+    #b_in_range = cv2.inRange(hsv_img, low_b_range, up_b_range)
+    r_in_range1 = cv2.inRange(hsv_img, low_r_range1, up_r_range1)
+    r_in_range2 = cv2.inRange(hsv_img, low_r_range2, up_r_range2)
+    r_in_range = cv2.bitwise_or(r_in_range1, r_in_range2)
+
+    # Binary morphological operation
+    #b_mask = cv2.morphologyEx(b_in_range, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
+    r_mask = cv2.morphologyEx(r_in_range, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
     
-    gray_img = None
+    return r_mask
+    
 
-    return gray_img
-
-def extra_area(gray_img):
+def extract_area(img, binary_img):
     images = []
-    return images
+    coords = []
+
+    # Find contours in the mask
+    conts = cv2.findContours(binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    # For each contour
+    for cont in conts:
+        # bounding box
+        rect = cv2.boundingRect(cont)
+
+        # area verification
+        area = rect[2] * rect[3] 
+        if area < 100:
+            continue
+        # ratio verification
+        ratio = rect[2] / rect[3]
+        if ratio > 10 or ratio < 0.1:
+            continue
+
+        # store image and coordinates (x, y, height, width)
+        sliced_img = img[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]]
+
+        cv2.imshow(" ", sliced_img)
+        cv2.waitKey(0)
+
+        images.append(sliced_img)
+        coords.append(rect)
+
+    return images, coords
 
 
 def svm_classify(img):
     result = []
-    return result
+    return True
 
 
 def hsv_svm(img):
@@ -33,14 +79,14 @@ def hsv_svm(img):
     if nothing is found, return an empty list []
     '''
     # Extract ROI
-    gray_img = pre_processing(img)
-    images = extra_area(gray_img)
+    binary_img = pre_processing(img)
+    images, coords = extract_area(img, binary_img)
     # Check each possible region
     found = []
-    for image in images:
+    for i, image in enumerate(images):
         result = svm_classify(image)
-        if result is not None:
-            found.append(found)
+        if result:
+            found.append(coords[i])
 
     return found
 
@@ -68,4 +114,7 @@ def main():
 
 if __name__ == "__main__":
     #main()
-    cv2.imread("lisa_dataset/images/stop_1323804701.avi_image3.png")
+    img = cv2.imread("demo/stop_sign.jpg")
+    #cv2.imshow("img", img)
+    #cv2.waitKey(0)
+    print(hsv_svm(img))
